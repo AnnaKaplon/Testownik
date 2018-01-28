a600da898205ab840534b17284943dafd4350843
Anna Kaplon <anna.kaplon19@o2.pl>
2018-01-28 14:55:05 +0100

    Initial commit

---
+++ GetData.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from random import shuffle
+
+class Question():
+    
+    def __init__(self, question, good_answer, wrong_answers):
+        self.question = question
+        self.good_answer = good_answer
+        self.wrong_answers = wrong_answers
+        
+    def getAllAnswers(self):
+        return [self.good_answer] + self.wrong_answers
+    
+
+def getData():
+    df = pd.read_csv('Example.csv', encoding = 'windows-1250')
+    questions = []
+    for _, row in df.iterrows():
+        wrong_answers_list = [answer for (key, answer) in row.items() if key != 'question' and key != 'good_answer']
+        questions.append(Question(row['question'], row['good_answer'], wrong_answers_list))
+    shuffle(questions)
+    return questions
+
+getData()
+
---
+++ README.md
@@ -0,0 +1,12 @@
+
+# README
+
+### Introduction
+
+Application made to support the learning process. It reads questions and answers from file, imitates school test and endly gives you number of good and wrong responses. 
+
+### Questions file format
+
+Data file has to have `Questions.csv` name and csv format. Columns with questions and good answers have to be named `question` and `good_answer`. Columns with other headers will be considered as wrong answers.
+
+`Example.csv` contains examples data.
