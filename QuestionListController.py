974a18075a4b28263c380a8f0bc84e75a7425a67
Anna Kaplon <anna.kaplon19@o2.pl>
2018-09-08 21:59:50 +0200

    adding getters to extract current question and answers

--- QuestionListController.py
+++ QuestionListController.py
@@ -1,12 +1,17 @@
 from Question import Question, getData
 
-class QuestionListController()
+class QuestionListController():
 
-	def __init__(self, questions):
-		self.questions = questions
+    def __init__(self, questions):
+        self.questions = questions
+        self.current_question = self.questions[0]
         self.counter = 1
         self.wrong_points = 0
         self.good_points = 0
         self.checked = False
-		
-	
+        
+    def getCurrentQuestion(self):
+        return self.current_question.question
+    
+    def getCurrentAnswers(self):
+        return self.current_question.getAllAnswers()
