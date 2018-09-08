ad75487bd3a978db37decd5cc021fe2f32a56fb9
Anna Kaplon <anna.kaplon19@o2.pl>
2018-09-08 22:43:56 +0200

    adding completed field and corresponding getter

--- QuestionListController.py
+++ QuestionListController.py
@@ -9,9 +9,19 @@ class QuestionListController():
         self.wrong_points = 0
         self.good_points = 0
         self.checked = False
+        self.completed = False
         
     def getCurrentQuestion(self):
         return self.current_question.question
     
     def getCurrentAnswers(self):
         return self.current_question.getAllAnswers()
+    
+    def getCompleted(self):
+        return self.completed
+    
+    def loadNewQuestion(self):
+        if self.counter < len(self.questions):
+            self.current_question = self.questions[self.counter]
+        else:
+            self.completed = True
